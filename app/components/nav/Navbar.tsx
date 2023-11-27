b5912336f1e76117f7463645915efb893b56522a
Raihan <raihanhn20@gmail.com>
2023-11-27 23:05:48 +0600

    container created for all component

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import Container from "../Container";
+import Link from "next/link";
 
-export default function Navbar() {
-  return <div>Navbar</div>;
-}
+const Navbar = () => {
+  return (
+    <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
+      <div className="py-4 border-b-[1px] ">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Link href={"/"}>E-Shop</Link>
+            <div className="hidden md:block">Search</div>
+            <div className=" flex items-center gap-8 md:gap-12 ">
+              <div className="">CartCount</div>
+              <div className="">UserMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
