3f1b2868dddce36d4aa2be1ccee9b5a30deaf52a
Raihan <raihanhn20@gmail.com>
2023-11-28 09:36:26 +0600

    Footer completed

---
+++ app/components/nav/HomeBanner.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomeBanner = () => {
+  return (
+    <div className="relative">
+      <div className="">
+        <div className=""></div>
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner;
--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Container from "../Container";
 import Link from "next/link";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
 const Navbar = () => {
   return (
@@ -8,7 +11,12 @@ const Navbar = () => {
       <div className="py-4 border-b-[1px] ">
         <Container>
           <div className="flex items-center justify-between gap-3 md:gap-0">
-            <Link href={"/"}>E-Shop</Link>
+            <Link
+              className={`${redressed.className} font-bold text-2xl`}
+              href={"/"}
+            >
+              E-Shop
+            </Link>
             <div className="hidden md:block">Search</div>
             <div className=" flex items-center gap-8 md:gap-12 ">
               <div className="">CartCount</div>
