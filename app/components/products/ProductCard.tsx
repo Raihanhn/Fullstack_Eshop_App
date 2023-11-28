d74b0a476f33052df373ef9395e72899c597c7d6
Raihan <raihanhn20@gmail.com>
2023-11-28 23:51:14 +0600

    product cart and dynamic route added

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { formatPrice } from "@/app/utils/formatPrice";
 import { truncateText } from "@/app/utils/truncateText";
+import { Rating } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 interface ProductCardProps {
@@ -7,21 +9,26 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const productRating =
+    data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
+    data.reviews.length;
   return (
     <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
       <div className="flex flex-col items-center w-full gap-1">
         <div className="aspect-square overflow-hidden relative w-full">
           <Image
             fill
-            className="w-full h-full object-contain"
             src={data.images[0].image}
             alt={data.name}
+            className="w-full h-full object-contain"
           />
         </div>
         <div className="mt-4">{truncateText(data.name)}</div>
-        <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
+        <div className="">
+          <Rating value={productRating} readOnly />
+        </div>
+        <div className="">{data.reviews.length} reviews </div>
+        <div className="font-semibold">{formatPrice(data.price)} </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
         <div className="">
           <HomeBanner />
         </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product: any) => {
+            return <ProductCard key={product.id} data={product} />;
+          })}
+        </div>
       </Container>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {products.map((product: any) => {
-          return <ProductCard key={product.id} data={product} />;
-        })}
-      </div>
     </div>
   );
 }
