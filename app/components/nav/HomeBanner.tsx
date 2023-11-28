171e9cb974874e9fed799fedb121b68b2a1283da
Raihan <raihanhn20@gmail.com>
2023-11-28 21:39:28 +0600

    product added

--- app/components/nav/HomeBanner.tsx
+++ app/components/nav/HomeBanner.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const HomeBanner = () => {
@@ -15,7 +16,14 @@ const HomeBanner = () => {
             GET 50% OFF
           </p>
         </div>
-        <div className=""></div>
+        <div className="w-1/3 relative aspect-video">
+          <Image
+            src="/banner-image.png"
+            fill
+            alt="Banner Image"
+            className="object-contain"
+          />
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Container from "./components/Container";
 import HomeBanner from "./components/nav/HomeBanner";
+import ProductCard from "./components/products/ProductCard";
+import { products } from "./utils/products";
+import { truncateText } from "./utils/truncateText";
 
 export default function Home() {
   return (
@@ -9,6 +12,11 @@ export default function Home() {
           <HomeBanner />
         </div>
       </Container>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {products.map((product: any) => {
+          return <ProductCard key={product.id} data={product} />;
+        })}
+      </div>
     </div>
   );
 }
