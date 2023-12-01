a4aab72e9f901b3f6d1819b407367004806c18b1
Raihan <raihanhn20@gmail.com>
2023-12-01 21:06:46 +0600

    quantity increase decrease fnc added

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,5 +1,7 @@
 "use client";
+import Button from "@/app/components/Button";
 import SetColor from "@/app/components/products/SetColor";
+import SetQuantity from "@/app/components/products/SetQuantity";
 import { Rating } from "@mui/material";
 import { useCallback, useState } from "react";
 
@@ -52,6 +54,27 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
     /* eslint-disable */
     [cartProduct.selectedImg]
   );
+
+  const handleQtyIncrease = useCallback(() => {
+    if (cartProduct.quantity === 99) {
+      return;
+    }
+
+    setCartProduct((prev) => {
+      return { ...prev, quantity: ++prev.quantity };
+    });
+  }, [cartProduct]);
+
+  const handleQtyDecrease = useCallback(() => {
+    if (cartProduct.quantity === 1) {
+      return;
+    }
+
+    setCartProduct((prev) => {
+      return { ...prev, quantity: --prev.quantity };
+    });
+  }, [cartProduct]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
       <div className=""></div>
@@ -89,9 +112,15 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
           />{" "}
         </div>
         <Horizontal />
-        <div>quality</div>
+        <SetQuantity
+          cartProduct={cartProduct}
+          handleQtyIncrease={handleQtyIncrease}
+          handleQtyDecrease={handleQtyDecrease}
+        />
         <Horizontal />
-        <div>add to cart</div>
+        <div>
+          <Button label="Add To Cart" onClick={() => {}} />{" "}
+        </div>
       </div>
     </div>
   );
