a72ca33dd9e9c94088c19d49fe7ee58c84586cc6
Raihan <raihanhn20@gmail.com>
2023-12-08 22:34:51 +0600

    clear cart function added

--- app/cart/ItemContent.tsx
+++ app/cart/ItemContent.tsx
@@ -13,7 +13,11 @@ interface ItemContentProps {
 }
 
 const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
-  const { handleRemoveProductFromCart } = useCart();
+  const {
+    handleRemoveProductFromCart,
+    handleCartQtyIncrease,
+    handleCartQtyDecrease,
+  } = useCart();
   return (
     <div className="grid grid-cols-5 text-xs md:text-sm gap-4 border-t-[1.5px] border-slate-200 py-4 items-center ">
       <div className="col-span-2 justify-self-start flex gap-2 md:gap-4">
@@ -46,8 +50,12 @@ const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
         <SetQuantity
           cartCounter={true}
           cartProduct={item}
-          handleQtyIncrease={() => {}}
-          handleQtyDecrease={() => {}}
+          handleQtyIncrease={() => {
+            handleCartQtyIncrease(item);
+          }}
+          handleQtyDecrease={() => {
+            handleCartQtyDecrease(item);
+          }}
         />
       </div>
       <div className="justify-self-end font-semibold">
