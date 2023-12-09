4c65961e7a52dd4c82e366389c96d16cec087acd
Raihan <raihanhn20@gmail.com>
2023-12-09 23:44:26 +0600

    react-hook-form added

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "../Container";
 import Link from "next/link";
 import { Redressed } from "next/font/google";
+import CartCount from "./CartCount";
 
 const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
@@ -19,7 +20,7 @@ const Navbar = () => {
             </Link>
             <div className="hidden md:block">Search</div>
             <div className=" flex items-center gap-8 md:gap-12 ">
-              <div className="">CartCount</div>
+              <CartCount />
               <div className="">UserMenu</div>
             </div>
           </div>
