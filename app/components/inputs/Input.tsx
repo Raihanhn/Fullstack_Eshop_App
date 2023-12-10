09a1cdb01fc64f65d54beefdf7741fd5e7c55a5e
Raihan <raihanhn20@gmail.com>
2023-12-10 23:50:48 +0600

    sign up form added

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -38,8 +38,10 @@ const Input: React.FC<InputProps> = ({
       />
       <label
         htmlFor={id}
-        className="absolute cursor-text duration-150 transform -translate-y-3 top-5
-       z-10 origin-[0] left-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:translate-y-4 "
+        className={`absolute cursor-text duration-150 transform -translate-y-3 top-5
+        z-10 origin-[0] left-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4
+        ${errors[id] ? "focus:text-rose-500" : "text-slate-400"}
+        `}
       >
         {label}{" "}
       </label>
