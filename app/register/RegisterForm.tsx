46d389c1e8748ca8c24a5adb5f318d125f499223
Raihan <raihanhn20@gmail.com>
2023-12-10 17:09:50 +0600

    register page added

---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import Heading from "../components/Heading";
+
+const RegisterForm = () => {
+  return (
+    <>
+      <Heading title="sign up for E~Shop" />
+      <hr className="bg-slate-300 w-full h-px" />
+    </>
+  );
+};
+
+export default RegisterForm;
