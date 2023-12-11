29705583cb45d2493ca9e165eb9fcc04da6d22ff
Raihan <raihanhn20@gmail.com>
2023-12-11 22:38:55 +0600

    next-auth added

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Heading from "../components/Heading";
+import Input from "../components/inputs/Input";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import Button from "../components/Button";
+import Link from "next/link";
+import { AiOutlineGoogle } from "react-icons/ai";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Heading title="sign in to for E~Shop" />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={AiOutlineGoogle}
+        onClick={() => {}}
+      />
+      <hr className="bg-slate-300 w-full h-px" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="password"
+      />
+      <Button
+        label={isLoading ? "Loading" : "Login"}
+        onClick={handleSubmit(onSubmit)}
+      />
+
+      <p className="text-sm">
+        Do not have an account?{" "}
+        <Link href={"/register"} className="underline">
+          Sign Up
+        </Link>{" "}
+      </p>
+    </>
+  );
 };
 
 export default LoginForm;
