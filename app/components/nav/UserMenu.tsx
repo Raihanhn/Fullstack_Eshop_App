67d98ff9481583a34c5d1bef21d674a483e83590
Raihan <raihanhn20@gmail.com>
2023-12-15 00:50:25 +0600

    stripe payment added

--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -27,7 +27,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           className="p-2 border-[1px] border-slate-400 flex flex-row items-center gap-1 rounded-full cursor-pointer hover:shadow-md transition text-slate-700"
           onClick={toggleOpen}
         >
-          <Avatar />
+          <Avatar src={currentUser?.image} />
           <AiFillCaretDown />
         </div>
         {isOpen && (
