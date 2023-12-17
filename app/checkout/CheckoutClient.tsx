7bd9f01c7be8076a16fdca812c1b343e2a7b35f7
Raihan <raihanhn20@gmail.com>
2023-12-17 21:32:47 +0600

    stripe-webhooks added

--- app/checkout/CheckoutClient.tsx
+++ app/checkout/CheckoutClient.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import React, { useCallback, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import CheckoutForm from "./CheckoutForm";
+import Button from "../components/Button";
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
@@ -77,6 +78,21 @@ const CheckoutClient = () => {
             handleSetPaymentSuccess={handleSetPaymentSuccess}
           />
         </Elements>
+      )}
+      {loading && <div className="text-center">Loading Checkout...</div>}
+      {error && (
+        <div className="text-center text-rose-500">Something went wrong...</div>
+      )}
+      {paymentSuccess && (
+        <div className="flex items-center flex-col gap-4">
+          <div className="text-teal-500 text-center">Payment Success</div>
+          <div className="max-w-[220px] w-full ">
+            <Button
+              label="View Your Orders"
+              onClick={() => router.push("/order")}
+            />
+          </div>
+        </div>
       )}
     </div>
   );
