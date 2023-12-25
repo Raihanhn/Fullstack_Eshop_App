f79028e1cbb2b7362ef83e2090971d8f2702786b
Raihan <raihanhn20@gmail.com>
2023-12-26 01:17:05 +0600

    BarGraph added

---
+++ app/admin/BarGraph.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+
+interface BarGraphProps {
+  data: GraphData[];
+}
+
+type GraphData = {
+  day: string;
+  date: string;
+  totalAmount: number;
+};
+
+const BarGraph: React.FC<BarGraphProps> = ({ data }) => {
+  console.log(data);
+  return <div></div>;
+};
+
+export default BarGraph;
